58cb20187caee0fd0adb9bf4010b777245c16760
SAYUK09 <sayurikamble123@gmail.com>
2020-12-31 11:27:54 +0530

    css & js

---
+++ script.js
@@ -0,0 +1,11 @@
+
+    console.log("what the hell")
+    const hamburger = document.getElementById('hamburger');
+    const navUl = document.getElementById('nav-ul')
+    console.log(hamburger)
+    
+    hamburger.addEventListener('click',()=>{
+        navUl.classList.toggle('show')
+    })
+
+
